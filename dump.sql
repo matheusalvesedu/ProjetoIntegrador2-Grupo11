5cc10e323fe8cc098ca54f575dc31ef86c27d4f2
felipecorsopretoni <felipe.cp5@puccampinas.edu.br>
2024-10-20 14:01:06 -0300

    mudança no arquivo dump

--- dump.sql
+++ dump.sql
@@ -1,21 +1,20 @@
-DROP TABLE users;
+DROP TABLE ACCOUNTS;
 
-CREATE TABLE users (
+CREATE TABLE ACCOUNTS (
     id INT PRIMARY KEY,       -- Identificador único do usuário
     name VARCHAR(100) NOT NULL,  -- Nome do usuário
     email VARCHAR(100) UNIQUE NOT NULL, -- Email, deve ser único
-    password VARCHAR(255) NOT NULL,    -- Senha criptografada
-    birthdate DATE NOT NULL           -- Data de nascimento
+    password VARCHAR(255) NOT NULL -- Senha do usuário
 );
 
--- CREATE SEQUENCE SEQ_ACCOUNTS START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE SEQ_ACCOUNTS START WITH 1 INCREMENT BY 1;
 
--- INSERT INTO ACCOUNTS (ID, COMPLETE_NAME, EMAIL, PASSWORD
--- ) VALUES (
---     SEQ_ACCOUNTS.NEXTVAL,
---     'Felipe Pretoni',
---     'felipe@felipe.com',
---     '123felipe'
--- )
+INSERT INTO ACCOUNTS (ID, COMPLETE_NAME, EMAIL, PASSWORD
+) VALUES (
+    SEQ_ACCOUNTS.NEXTVAL,
+    'Felipe Pretoni',
+    'felipe@felipe.com',
+    '123felipe'
+)
 
--- commit;
+commit;
